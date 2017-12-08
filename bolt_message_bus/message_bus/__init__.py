a752e26e64137a48ca0ce85c002025188149bca4
Saurabh Badhwar <sbadhwar@redhat.com>
2017-12-08 15:45:09 +0530

    Change the project naming convention

---
+++ bolt_message_bus/message_bus/__init__.py
@@ -0,0 +1,9 @@
+"""Message server module for bolt message bus.
+
+Name: message_server
+Description: The message server for the bolt message bus
+Author: Saurabh Badhwar <sbadhwar@redhat.com>
+Date: 08/12/2017
+"""
+from bolt_message_bus.message_bus.connection_handler import \
+        ConnectionHandler
